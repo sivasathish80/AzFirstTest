feabab8b76635d6a74a7a296d5351ea26b62d7b6
sivasathish80 <sivasathish80@gmail.com>
2021-05-25 15:57:34 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,5 +3,18 @@
 echo `hostname`
 echo `pwd`
 echo "My Name is " $1
+echo "GIT Status"
+git status
 
+echo "GIT Checkout"
+git checkout
+
+echo "GIT Checkout to Master"
+git checkout master
+
+echo "Check the branch"
+git branch -v
+
+echo "Pull Files from master"
+git pull origin master
 
