97852edf57ed9519f2e8934423cfc0ca58cc935e
sivasathish80 <sivasathish80@gmail.com>
2021-05-25 16:08:15 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -21,6 +21,9 @@ git pull origin master
 echo "list files"
 ls -ltr
 
+echo "Grant execute permission for initiateBuild script"
+chmod 755 initiateBuild.sh
+
 echo "Execute initiateBuild script"
 ./initiateBuild.sh
 
